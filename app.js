67d2035b00fd1eafadfd838fc701066014c4a7d2
Constantin <constantin@xmanna.com>
2021-06-30 09:00:00 +0300

    Nodemon added

--- app.js
+++ app.js
@@ -14,9 +14,4 @@ app.get('/', (req, res) => {
   res.send('Testing app');
 });
 
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  // console.log('Test server is running');
-});
-
 module.exports = app;
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,12 @@
 const TodoModel = require('../models/todo.model');
 
-exports.createTodo = async (req, res) => {
-  const createdModel = await TodoModel.create(req.body);
-  res.status(201).json(createdModel);
+exports.createTodo = async (req, res, next) => {
+  try {
+    const createdModel = await TodoModel.create(req.body);
+    res.status(201).json(createdModel);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message)
+  }
+  
 };
